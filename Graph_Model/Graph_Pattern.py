069900abc46f58aec6659be2c461eaa4c4377c62
cyriac.azefack <azefacK1996>
2018-05-02 15:41:38 +0200

    Plot Probability matrix time evolution

--- Graph_Model/Graph_Pattern.py
+++ Graph_Model/Graph_Pattern.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 import datetime as dt
 from pprint import pprint
-from random import random
+from random import uniform
 from subprocess import check_call
 
-import matplotlib.image as mpimg
-# import matplotlib.pyplot as plt
+import Pattern2Graph
+import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
-import scipy.stats as st
+import seaborn as sns
 
 from xED_Algorithm.Candidate_Study import modulo_datetime
 
+sns.set_style("darkgrid")
 
 class Graph_Pattern:
     ID = 0
+    NB_PERIODS_SLIDING_WINDOW = 5
     START_NODE = "START PERIOD"
     END_NODE = "END PERIOD"
 
@@ -29,14 +31,38 @@ class Graph_Pattern:
         :param prob_matrix:
         :param wait_matrix:
         '''
-        self.nodes = nodes
+
+        self.nodes = nodes  # len(nodes) = n
         self.period = period
+        self.sliding_time_window = Graph_Pattern.NB_PERIODS_SLIDING_WINDOW * period
         self.mu = mu
         self.sigma = sigma
-        self.prob_matrix = prob_matrix
-        self.wait_matrix = wait_matrix
+        self.prob_matrix = prob_matrix  # size = (n x n)
+        self.time_matrix = wait_matrix  # size = (n-1 x n-1)
         self.ID = Graph_Pattern.ID
+        self.graph = None
+        self.time_evo_prob_matrix = None
+        self.time_evo_time_matrix = None
+
         Graph_Pattern.ID += 1
+
+    def __repr__(self):
+        '''
+        :return: Text description of a graph pattern
+        '''
+        txt = 'Graph Pattern ID N°{}\n'.format(self.ID)
+        txt += 'Nodes : {}\n'.format(self.nodes)
+        txt += 'Period : {}\n'.format(self.period)
+        txt += 'Mean Time : {}\n'.format(dt.timedelta(seconds=self.mu))
+        txt += 'Std Time : {}\n'.format(dt.timedelta(seconds=self.sigma))
+
+        return txt
+
+    def get_prob_matrix(self):
+        return self.prob_matrix
+
+    def get_time_matrix(self):
+        return self.time_matrix
 
     def display(self, output_folder, debug=False):
         td = dt.timedelta(seconds=self.mu)
@@ -48,7 +74,7 @@ class Graph_Pattern:
         self.draw_directed_graph(filename=output_folder + filename,
                                  title=title, debug=debug)
 
-    def getIndex(self):
+    def get_index(self):
         return self.ID
 
     def draw_directed_graph(self, filename, title, debug=False):
@@ -91,7 +117,7 @@ class Graph_Pattern:
             if tmp_origin != "END_PERIOD" or tmp_destination != "END_PERIOD":
                 G.add_edge(tmp_origin, tmp_destination, weight=v, penwidth=2 if v > 0.5 else 1, label=v,
                            color='blue' if v > 0.5 else 'black',
-                           headlabel=self.wait_matrix[self.nodes.index(tmp_origin)][self.nodes.index(tmp_destination)])
+                           headlabel=self.time_matrix[self.nodes.index(tmp_origin)][self.nodes.index(tmp_destination)])
             else:
                 G.add_edge(tmp_origin, tmp_destination, weight=v, penwidth=2 if v > 0.5 else 1, label=v,
                            color='blue' if v > 0.5 else 'black')
@@ -110,12 +136,8 @@ class Graph_Pattern:
 
         nx.drawing.nx_pydot.write_dot(G, filename + '.dot')
         check_call(['dot', '-Tpng', filename + '.dot', '-o', filename + '.png'])
-        if debug:
-            plt.clf()
-            plt.axis('off')
-            img = mpimg.imread(filename + '.png')
-            plt.imshow(img)
-            plt.show()
+
+        self.graph = G
 
     def get_markov_edges(self, Q):
         edges = {}
@@ -126,6 +148,9 @@ class Graph_Pattern:
         return edges
 
     def simulate(self, start_date, end_date):
+
+        self.extrapolate_time_evolution(start_date, end_date)
+
         result = pd.DataFrame(columns=["date", "label"])
 
         # The real 'start_date' is the beginning of the next period
@@ -142,11 +167,13 @@ class Graph_Pattern:
             cs_row = np.cumsum(row)
 
             # Pick a random number between 0 -- 1
-            rand = random()
+
             destination_index = None
 
+            rand = uniform(0, cs_row[-1])
+
             for index in range(len(self.nodes)):
-                if rand < cs_row[index]:
+                if rand <= cs_row[index]:
                     destination_index = index
                     break
 
@@ -160,18 +187,13 @@ class Graph_Pattern:
                 destination_date = current_date
             else:
                 # Now we have to compute the waiting time to get to the destination state
-                tuple = self.wait_matrix[state_index][destination_index]
-                dist_name = tuple[0]
-                param = tuple[1:][0]
-                dist = getattr(st, dist_name)
-                # Separate parts of parameters
-                arg = param[:-2]
-                loc = param[-2]
-                scale = param[-1]
+                tuple = self.time_matrix[state_index][destination_index]
+                mu = tuple[0]
+                sigma = tuple[1]
 
                 while True:
                     try:
-                        waiting_time = int(dist.rvs(loc=loc, scale=scale, *arg))
+                        waiting_time = int(sigma * np.random.randn() + mu)
                         # Compute the destination date
                         destination_date = current_date + dt.timedelta(seconds=waiting_time)
                         # Add the event to the result
@@ -180,10 +202,85 @@ class Graph_Pattern:
                     except:
                         print("OOOps ! Date Overflow. Let's try again...")
 
-
-
-
             current_state = destination_state
             current_date = destination_date
 
         return result
+
+    def compute_time_evolution(self, data):
+        '''
+        Compute the pattern time evolution by sliding a time window through the original data
+        :param data:
+        :return:
+        '''
+
+        n = len(self.nodes)
+        time_evo_prob_matrix = [[pd.DataFrame(columns=['probability']) for j in range(n)] for i in
+                                range(n)]  # Date as index
+        time_evo_time_matrix = [[pd.DataFrame(columns=['mean_time', 'sigma_time']) for j in range(n - 1)] for i in
+                                range(n - 1)]  # Date as index
+
+        start_date = data.date.min().to_pydatetime()
+        # We start at the beginning of the first period
+        start_date = start_date - dt.timedelta(seconds=modulo_datetime(start_date, self.period))
+        end_date = data.date.max().to_pydatetime()
+        # We take the time window into account for the end_date
+        end_date = end_date - self.sliding_time_window
+
+        current_start_date = start_date
+
+        nodes = self.nodes[1:-1]  # We remove 'START_NODE' and 'END_NODE'
+        while current_start_date < end_date:
+            current_end_date = current_start_date + self.sliding_time_window
+            time_description = {self.mu: self.sigma}
+            graph_pattern = Pattern2Graph.pattern2graph(data, labels=nodes, time_description=time_description,
+                                                        period=self.period, start_date=current_start_date,
+                                                        end_date=current_end_date, display_graph=False)
+
+            if len(graph_pattern) == 0:
+                current_start_date += self.period
+                continue
+            graph_pattern = graph_pattern[0]
+            prob_matrix = graph_pattern.get_prob_matrix()
+            time_matrix = graph_pattern.get_time_matrix()
+
+            # Fill time_evo_prob_matrix
+            for i in range(n):
+                for j in range(n):
+                    prob_df = time_evo_prob_matrix[i][j]
+                    prob_df.loc[current_start_date] = [prob_matrix[i, j]]
+
+                    if i < n - 1 and j < n - 1:
+                        if time_matrix[i][j]:
+                            time_df = time_evo_time_matrix[i][j]
+                            time_df.loc[current_start_date] = [time_matrix[i][j][0], time_matrix[i][j][1]]
+
+            current_start_date += self.period
+
+        self.time_evo_prob_matrix = time_evo_prob_matrix
+        self.time_evo_time_matrix = time_evo_time_matrix
+
+    def extrapolate_time_evolution(self, start_date, end_date):
+        '''
+        Compute the probability transition matrix and the waiting time transition matrix by extrapolating time evolution
+        :param start_date:
+        :param end_date:
+        :return: (prob_matrix, time_matrix)
+        '''
+
+        n = len(self.nodes)
+        extr_prob_matrix = np.zeros((n, n))
+        extr_time_matrix = [[[] for j in range(n - 1)] for i in range(n - 1)]  # Empty lists
+
+        for i in range(n):
+            fig, ax = plt.subplots()
+            for j in range(n):
+                df = self.time_evo_prob_matrix[i][j]
+                txt = "--> [{}]".format(self.nodes[j])
+                ax.plot_date(df.index, df.probability, label=txt, linestyle="-")
+            ax.set_ylabel('Transition probability')
+            ax.set_xlabel('Date')
+            ax.set_title('From Node [{}]'.format(self.nodes[i]))
+            ax.legend()
+            plt.gcf().autofmt_xdate()
+            plt.show()
--- Graph_Model/Pattern2Graph.py
+++ Graph_Model/Pattern2Graph.py
@@ -4,59 +4,79 @@ Created on Wed Mar 28 11:22:58 2018
 
 @author: cyriac.azefack
 """
-from collections import defaultdict
 
+import Graph_Pattern
 import scipy.stats as st
 
-from Graph_Model.Graph_Pattern import Graph_Pattern
 from xED_Algorithm.Candidate_Study import *
 from xED_Algorithm.xED_Algorithm import *
 
 
 def main():
-    letters = ['A', 'C']
-    dataset_type = 'label'
+    dataset_name = 'KA'
 
-    for letter in letters:
-        dataset = pick_dataset(letter, dataset_type)
+    dataset = pick_dataset(name=dataset_name)
 
-        output = "output/K{} House/{}".format(letter, dataset_type)
-        patterns = pickle.load(open(output + '/patterns.pickle', 'rb'))
+    output = "../output/{}".format(dataset_name)
+    patterns = pickle.load(open(output + '/patterns.pickle', 'rb'))
 
-        pattern_graph_list = []
-        for _, pattern in patterns.iterrows():
+    # Only keep the first pattern
+    patterns.drop(patterns.index[3:], inplace=True)
+    pattern_graph_list = []
+    for _, pattern in patterns.iterrows():
 
-            labels = list(pattern['Episode'])
-            period = pattern['Period']
-            validity_start_date = pattern['Start Time'].to_pydatetime()
-            validity_end_date = pattern['End Time'].to_pydatetime()
-            validity_duration = validity_end_date - validity_start_date
-            nb_periods = validity_duration.total_seconds() / period.total_seconds()
-            description = pattern['Description']
-            output_folder = output + "/Patterns_Graph/" + "_".join(labels) + "/"
+        labels = list(pattern['Episode'])
+        period = pattern['Period']
+        validity_start_date = pattern['Start Time'].to_pydatetime()
+        validity_end_date = pattern['End Time'].to_pydatetime()
+        validity_duration = validity_end_date - validity_start_date
+        nb_periods = validity_duration.total_seconds() / period.total_seconds()
+        description = pattern['Description']
+        output_folder = output + "/Patterns_Graph/" + "_".join(labels) + "/"
 
-            if not os.path.exists(os.path.dirname(output_folder)):
-                try:
-                    os.makedirs(os.path.dirname(output_folder))
-                except OSError as exc:  # Guard against race condition
-                    if exc.errno != errno.EEXIST:
-                        raise
+        if not os.path.exists(os.path.dirname(output_folder)):
+            try:
+                os.makedirs(os.path.dirname(output_folder))
+            except OSError as exc:  # Guard against race condition
+                if exc.errno != errno.EEXIST:
+                    raise
 
-            mini_list = pattern2graph(data=dataset, labels=labels, time_description=description, period=period,
-                                      start_date=validity_start_date, end_date=validity_end_date,
-                                      output_directory=output_folder, debug=False)
+        start_time = t.process_time()
+        mini_list = pattern2graph(data=dataset, labels=labels, time_description=description, period=period,
+                                  start_date=validity_start_date, end_date=validity_end_date,
+                                  output_directory=output_folder, display_graph=False)
+        elapsed_time = dt.timedelta(seconds=round(t.process_time() - start_time, 1))
 
-            pattern_graph_list += mini_list
+        print("\n")
+        print("###############################")
+        print("Patterns turned into graphs. Elapsed time : {}".format(elapsed_time))
+        print("##############################")
+        print("\n")
+
+        start_date = dataset.date.max().to_pydatetime()
+        end_date = start_date + dt.timedelta(days=5)
+        # Compute Time Evolution
+        for pattern_graph in mini_list:
+            pattern_graph.compute_time_evolution(dataset)
+            filename = output_folder + "/Graph_Pattern_Object.pkl".format(pattern_graph.ID)
+            with open(filename, 'wb') as f:
+                pickle.dump(pattern_graph, f, protocol=pickle.HIGHEST_PROTOCOL)
+            sim = pattern_graph.simulate(start_date, end_date)
+
+        pattern_graph_list += mini_list
 
 
 def pattern2graph(data, labels, time_description, period, start_date, end_date, tolerance_ratio=2, Tep=30,
-                  output_directory='./', debug=False):
+                  output_directory='./', display_graph=False):
     '''
     Turn a pattern to a graph
     :param data: Input dataset
     :param labels: list of labels included in the pattern
     :param time_description: time description of the pattern {mu1 : sigma1, mu2 : sigma2, ...}
     :param tolerance_ratio: tolerance ratio to get the expected occurrences
+    :param period : Periodicity of the time description
+    :param start_date :
+    :param end_date :
     :param Tep : [in Minutes] Maximal time interval between events in an episode occurrence. Should correspond to the maximal duration of the ADLs.
     :return: A list of <i>Graph_Pattern</i> (one for each time component)
     '''
@@ -69,10 +89,7 @@ def pattern2graph(data, labels, time_description, period, start_date, end_date, 
         Mp = np.zeros((n, n))
 
         # n-1 x n-1 edges for waiting time transition laws (no wait time to END NODE)
-        Mwait = defaultdict(lambda: defaultdict(list))
-        for i in range(n - 1):
-            for j in range(n - 1):
-                Mwait[i][j] = []
+        Mwait = [[[] for j in range(n - 1)] for i in range(n - 1)]  # Empty lists
 
         # Find pattern occurrences
         occurrences = find_occurrences(data, tuple(labels), Tep)
@@ -109,7 +126,6 @@ def pattern2graph(data, labels, time_description, period, start_date, end_date, 
         # We always have the EDGE END --> START
         # TODO : EDGE END ----> START is putted as 1 (for check purporses)
         Mp[n - 1, 0] = 1
-
 
         # Missing occurrences, #START --> END directly (waiting time = Period)
         # TODO : Totally missing occurrences
@@ -159,22 +175,20 @@ def pattern2graph(data, labels, time_description, period, start_date, end_date, 
             if abs(s_row - 1) > tol:
                 raise ValueError('The sum of the row {} is not 1 : {}'.format(i, s_row))
 
-
-        # Find the best fitting probability distribution law for all the waiting times
         for i in range(n - 1):
             for j in range(n - 1):
                 array = np.asarray(Mwait[i][j])
                 if len(array) > 0:
                     # Normal distribution by default
-                    Mwait[i][j] = ('norm', (np.mean(array), np.std(array)))
+                    Mwait[i][j] = (np.mean(array), np.std(array))
                 else:
                     Mwait[i][j] = None
 
-        pattern_graph = Graph_Pattern(nodes, period, mu, sigma, Mp, Mwait)
+        pattern_graph = Graph_Pattern.Graph_Pattern(nodes, period, mu, sigma, Mp, Mwait)
         pattern_graph_list.append(pattern_graph)
 
-        if debug:
-            pattern_graph.display(output_folder=output_directory, debug=debug)
+        if display_graph:
+            pattern_graph.display(output_folder=output_directory, debug=display_graph)
 
     return pattern_graph_list
 
